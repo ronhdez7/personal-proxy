14f8a3c30f0449b919f414a6a35650e506fd8030
ronald hernandez <ronaldhp2007@gmail.com>
2024-09-15 16:25:14 -0400

    better logging

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const cors = require("cors");
 
 const PORT = process.env.PORT;
 
+app.use((req, res, next) => {
+  console.log("HIT -", req.method, req.url);
+  next();
+});
+
 app.use(cors());
 
 app.get("/hello", (req, res) => {
@@ -14,7 +19,7 @@ app.get("/hello", (req, res) => {
 app.use(
   proxy((req) => {
     const hostname = req.hostname;
-    console.log("HIT:", hostname);
+    console.log("PROXIED -", req.method, hostname);
     return hostname;
   })
 );
